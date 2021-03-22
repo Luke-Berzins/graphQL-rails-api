986b6b1fa35d40ef4c9669f8a6982e0bcf4b707a
Luke Berzins <lukeberzins16@gmail.com>
2021-03-22 14:43:08 -0400

    testing views/controllers with react frontend

--- Gemfile
+++ Gemfile
@@ -39,5 +39,7 @@ end
 
 
 gem 'graphql', '1.11.6'
+gem 'webpacker'
+gem 'react-rails'
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+
+  root 'pages#index'
+
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
   end
+
+ 
+
+  get "/articles", to: "articles#index"
   post "/graphql", to: "graphql#execute"
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
